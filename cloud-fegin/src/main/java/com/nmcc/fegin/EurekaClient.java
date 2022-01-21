27469c34cf97bc29cebd5b7e547b17d376ed9114
niemancun <niemancun@aipark.com>
2022-01-21 09:51:11 +0800

    fix

--- cloud-fegin/src/main/java/com/nmcc/fegin/EurekaClient.java
+++ cloud-fegin/src/main/java/com/nmcc/fegin/EurekaClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(url = "127.0.0.1:80", name = "cloud-eureka")
+@FeignClient(url = "${eureka.url}", name = "cloud-eureka")
 public interface EurekaClient {
 
 
